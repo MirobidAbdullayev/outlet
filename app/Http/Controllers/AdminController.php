ade0ea4ee7f567866ca41edaa51336724a7af0df
Mirobid Abdullayev <80286890+MirobidAbdullayev@users.noreply.github.com>
2023-05-02 22:56:24 +0500

    ....

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -39,27 +39,6 @@ class AdminController extends Controller
 
     public function add_product(Request $req)
    {
-    $req->validate([
-        'imageFile' => 'required',
-        'imageFile.*' => 'mimes:jpeg,jpg,png,gif,csv,txt,pdf|max:2048'
-      ]);
-  
-      if($req->hasfile('imageFile')) {
-          foreach($req->file('imageFile') as $file)
-          {
-              $name = $file->getClientOriginalName();
-              $file->move(public_path().'/uploads/', $name);  
-              $imgData[] = $name;  
-          }
-  
-          $fileModal = new Product();
-          $fileModal->name = json_encode($imgData);
-          $fileModal->image_path = json_encode($imgData);
-          
-         
-          $fileModal->save();
-  
-         return back()->with('success', 'File has successfully uploaded!');
-      }
+    
    }
 }
