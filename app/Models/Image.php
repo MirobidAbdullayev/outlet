f5d5d86a26f2860d0192c508449ed8c3907b9991
Mirobid Abdullayev <80286890+MirobidAbdullayev@users.noreply.github.com>
2023-05-07 23:45:44 +0500

    Laravel CRUD With Multiple Image Upload

---
+++ app/Models/Image.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
+
+class Image extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'image',
+        'post_id',
+    ];
+
+    public function products()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Image;
 
 class Product extends Model
 {
@@ -13,6 +14,27 @@ class Product extends Model
 
     protected $fillable = [
         'name',
-        'image_path'
+        'description',
+        'price',
+        'discount_price',
+        'quantity',
+        'category',
+        'color_1',
+        'color_2',
+        'color_3',
+        'color_4',
+        'color_5',
+        'color_6',
+        'sz_1',
+        'sz_2',
+        'sz_3',
+        'sz_4',
+        'sz_5',
+        'sz_6',
     ];
+
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
 }
