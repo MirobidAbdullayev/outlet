aa4573bb18ac1e1260f846576576ed78c220e2f8
Mirobid Abdullayev <80286890+MirobidAbdullayev@users.noreply.github.com>
2023-05-20 19:25:47 +0500

    ....

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,32 +9,32 @@ class ProductController extends Controller
 {
     public function products($category_id)
     {
-        $product=product::where('category_id', $category_id)->get();
+        $product=Product::where('category_id', $category_id)->get();
         
         return view('admin.view_products.products', compact('product'));
     }
 
     public function women_product($category_id)
     {
-        $product=product::where('category_id', $category_id)->get();
-        return view('admin.view_products.women_product');
+        $product=Product::where('category_id', $category_id)->get();
+        return view('admin.view_products.women_product', compact('product'));
     }
 
     public function children_product($category_id)
     {
-        $product=product::where('category_id', $category_id)->get();
+        $product=Product::where('category_id', $category_id)->get();
         return view('admin.view_products.children_product', compact('product'));
     }
 
     public function sports_product($category_id)
     {
-        $product=product::where('category_id', $category_id)->get();
+        $product=Product::where('category_id', $category_id)->get();
         return view('admin.view_products.sports_product', compact('product'));
     }
 
     public function special_product($category_id)
     {
-        $product=product::where('category_id', $category_id)->get();
+        $product=Product::where('category_id', $category_id)->get();
         return view('admin.view_products.special_product', compact('product'));
     }
 }
