9c0635423a0f72624d70c1471f49d91d3a86b88c
Mirobid Abdullayev <80286890+MirobidAbdullayev@users.noreply.github.com>
2023-06-05 23:49:42 +0500

    update table

--- resources/views/admin/view_products/products_men.blade.php
+++ resources/views/admin/view_products/products_men.blade.php
@@ -21,39 +21,8 @@
             width: 50px;
             height: 50px;
         }
-        table {
-        border-collapse: collapse;
-        width: 100%;
-        font-family: Arial, sans-serif;
-        color: #444;
-        }
-
-        th, td {
-        text-align: left;
-        padding: 8px;
-        }
-
-        th {
-        background-color: #f2f2f2;
-        color: #333;
-        }
-
-        tr:nth-child(even) {
-        background-color: #f2f2f2;
-        }
-
-        tr:hover {
-        background-color: #ddd;
-        }
-
-        td:first-child {
-        border-top-left-radius: 5px;
-        border-bottom-left-radius: 5px;
-        }
-
-        td:last-child {
-        border-top-right-radius: 5px;
-        border-bottom-right-radius: 5px;
+        .th_deg{
+            padding: 2px;
         }
     </style>
   <body>
@@ -65,44 +34,35 @@
             <div class="card">
                 <h2 class="font_size">Erkaklar Mahsulotlari</h2>
                 <div class="card-body">
-                    <table>
-                        <thead>
-                            <tr>
-                            <th>Mahsulot nomi</th>
-                            <th>Tasnifi</th>
-                            <th>Narxi</th>
-                            <th>Chegirma Narxi</th>
-                            <th>Miqdori</th>
-                            <th>Rangi</th>
-                            <th>O'lchami</th>
-                            <th>Kategoriya_id</th>
-                            <th>Rasmi</th>
-                            <th>Delete</th>
-                            <th>Edit</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($product as $item)
-                            <tr>
-                                <td>{{ $item->name}}</td>
-                                <td>{{ $item->description}}</td>
-                                <td>{{ $item->price}}</td>
-                                <td>{{ $item->discount_price}}</td>
-                                <td>{{ $item->quantity}}</td>
-                                <td>{{ $item->color_1}}/{{ $item->color_2}}/{{ $item->color_3}}/{{ $item->color_4}}/{{ $item->color_5}}/{{ $item->color_6}}</td>
-                                <td>{{ $item->sz_1}}/{{ $item->sz_2}}/{{ $item->sz_3}}/{{ $item->sz_4}}/{{ $item->sz_5}}/{{ $item->sz_6}}</td>
-                                <td>{{ $item->category_id}}</td>
-                                @foreach ($item->images as $image)
-                                <td>
-                                    <img class="img_size" src="{{ asset('images/' .  $image->image_path)}}">
-                                </td> 
-                                @endforeach
-                                <td><a class="btn btn-danger" onclick="return confirm('Siz haqiqatdan ham manashu mahsulotni o\'chirmoqchimisiz?')" href="{{url('products_delete,$product->id')}}">Delete</a></td>
-                                <td><a class="btn btn-success" href="">Edit</a></td>   
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>                 
+                    <table class="center">
+                        <tr class="">
+                            <th class="th_deg">ID</th>
+                            <th class="th_deg">Mahsulot nomi</th>
+                            <th class="th_deg">Narxi</th>
+                            <th class="th_deg">Chegirma Narxi</th>
+                            <th class="th_deg">O'lchami</th>
+                            <th class="th_deg">Rangi</th>
+                            <th class="th_deg">Miqdori</th>
+                            <th class="th_deg">Kategoriya_id</th>
+                            <th class="th_deg">Delete</th>
+                            <th class="th_deg">Edit</th>
+                        </tr>
+                        @foreach ($product as $item)
+                        <tr>
+                            <td>{{ $item->id}}</td>
+                            <td>{{ $item->name}}</td>
+                            <td>{{ $item->price}}</td>
+                            <td>{{ $item->discount_price}}</td>
+                            <td>{{ $item->sz_1}}/{{ $item->sz_2}}/{{ $item->sz_3}}/{{ $item->sz_4}}/{{ $item->sz_5}}/{{ $item->sz_6}}</td>
+                            <td>{{ $item->color_1}}/{{ $item->color_2}}/{{ $item->color_3}}/{{ $item->color_4}}/{{ $item->color_5}}/{{ $item->color_6}}</td>
+                            <td>{{ $item->quantity}}</td>
+                            <td>{{ $item->category_id}}</td>
+                            <td><a class="btn btn-danger" onclick="return confirm('Siz haqiqatdan ham manashu mahsulotni o\'chirmoqchimisiz?')" href="{{url('products_delete,$product->id')}}">Delete</a></td>
+                            <td><a class="btn btn-success" href="">Edit</a></td>
+                            
+                        </tr>
+                        @endforeach
+                    </table>                  
                 </div>
             </div>
         </div>
--- resources/views/admin/view_products/products_special.blade.php
+++ resources/views/admin/view_products/products_special.blade.php
@@ -34,41 +34,35 @@
             <div class="card">
                 <h2 class="font_size">Mahsus Kiyimlar</h2>
                 <div class="card-body">
-                    <table class="center">
+                <table class="center">
                         <tr class="">
+                            <th class="th_deg">ID</th>
                             <th class="th_deg">Mahsulot nomi</th>
-                            <th class="th_deg">Tasnifi</th>
                             <th class="th_deg">Narxi</th>
                             <th class="th_deg">Chegirma Narxi</th>
-                            <th class="th_deg">Miqdori</th>
-                            <th class="th_deg">Rangi</th>
                             <th class="th_deg">O'lchami</th>
+                            <th class="th_deg">Rangi</th>
+                            <th class="th_deg">Miqdori</th>
                             <th class="th_deg">Kategoriya_id</th>
-                            <th class="th_deg">Rasmi</th>
                             <th class="th_deg">Delete</th>
                             <th class="th_deg">Edit</th>
                         </tr>
                         @foreach ($product as $item)
                         <tr>
+                            <td>{{ $item->id}}</td>
                             <td>{{ $item->name}}</td>
-                            <td>{{ $item->description}}</td>
                             <td>{{ $item->price}}</td>
                             <td>{{ $item->discount_price}}</td>
-                            <td>{{ $item->quantity}}</td>
-                            <td>{{ $item->color_1}}/{{ $item->color_2}}/{{ $item->color_3}}/{{ $item->color_4}}/{{ $item->color_5}}/{{ $item->color_6}}</td>
                             <td>{{ $item->sz_1}}/{{ $item->sz_2}}/{{ $item->sz_3}}/{{ $item->sz_4}}/{{ $item->sz_5}}/{{ $item->sz_6}}</td>
+                            <td>{{ $item->color_1}}/{{ $item->color_2}}/{{ $item->color_3}}/{{ $item->color_4}}/{{ $item->color_5}}/{{ $item->color_6}}</td>
+                            <td>{{ $item->quantity}}</td>
                             <td>{{ $item->category_id}}</td>
-                            @foreach ($item->images as $image)
-                            <td>
-                                <img class="img_size" src="{{ asset('images/' .  $image->image_path)}}">
-                            </td> 
-                            @endforeach
                             <td><a class="btn btn-danger" onclick="return confirm('Siz haqiqatdan ham manashu mahsulotni o\'chirmoqchimisiz?')" href="{{url('products_delete,$product->id')}}">Delete</a></td>
                             <td><a class="btn btn-success" href="">Edit</a></td>
                             
                         </tr>
                         @endforeach
-                    </table>                  
+                    </table>                    
                 </div>
             </div>
         </div>
